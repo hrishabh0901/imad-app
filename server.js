c677921cfe94358938137f00abdb0e58dbc53366
Hrishabh Singh <30730227+hrishabh0901@users.noreply.github.com>
2018-04-15 23:33:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user:'hrishabhsingh0901',
+    database:'hrishabhsingh0901',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password : process.envDB_PASSWORD
+};
+    
 
 var app = express();
 
@@ -9,6 +18,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    
+    pool.query('SELECT * FROM test',function(err,result){
+       
+        if(err)
+        {
+            res.status(500.send(err.toString()));
+        }
+        else
+        {
+            res.send(JSON.stringfy(result));
+        }
+    });
+    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
