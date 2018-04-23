38a55922658d3e52d67f324c92fd67e500fb5eee
Hrishabh Singh <30730227+hrishabh0901@users.noreply.github.com>
2018-04-23 23:49:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -180,3 +180,8 @@ app.get('/article-two',function(req,res){
 app.get('/article-three',function(req,res){
   res.sendFile(path.join(__dirname, 'ui','article-three.html'));
 });
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
