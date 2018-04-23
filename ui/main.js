d97f139e7a1819e9e0fc8474ce5910aa050611b1
Hrishabh Singh <30730227+hrishabh0901@users.noreply.github.com>
2018-04-23 23:31:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,33 @@
-var button  = document.getElementById('counter');
-var counter = 0;
-button.onclick = function()
-{
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+window.onload=function(){
+var submit=document.getElementById('submitb');
+submit.onclick=function(){
+
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                console.log('Logged in');
+                alert("logged in successfully");
+          
+            }
+            else if(request.status===403){
+                alert("Username/password is invalid");
+            }
+            else if(request.status===500){
+                alert("Something went wrong on server");
+            }
+        }
+    };
+ 
+     var username=document.getElementById('username').value;
+     var password=document.getElementById('password').value;
+    
+     
+     request.open('POST','http://duttaanimesh08.imad.hasura-app.io/login',true);
+     request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({username:username,password:password}));
+    
+};          
+    
 };
